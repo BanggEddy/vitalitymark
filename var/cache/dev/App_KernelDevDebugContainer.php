02b3b24ace946f289745c7690642fe33a233cb9c
David LI <108392457+BanggEddy@users.noreply.gitlab.com>
2024-03-15 10:30:19 +0100

    système panier finish for users

--- var/cache/dev/App_KernelDevDebugContainer.php
+++ var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,7 +16,7 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \ContainerOxEDHgz\App_KernelDevDebugContainer([
     'container.build_hash' => 'OxEDHgz',
-    'container.build_id' => 'aee3a2b1',
-    'container.build_time' => 1710461516,
+    'container.build_id' => '135007fe',
+    'container.build_time' => 1710494987,
     'container.runtime_mode' => \in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) ? 'web=0' : 'web=1',
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerOxEDHgz');
